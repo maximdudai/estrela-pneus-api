4ed6d0b132483296745a4009450ba0af0ac8ab87
maximdudai <maxim.dudai01@gmail.com>
2023-04-15 22:18:29 +0100

    install module jsonwebtoken for token
    generate a new token when user register

--- server.js
+++ server.js
@@ -2,22 +2,17 @@ import express from 'express'
 import cors from 'cors'
 import bodyParser from 'body-parser';
 const app = express();
-
-import dbConnection from './server/database/connection.js';
-
-import { onUserAuthentication } from './server/authentication/register/authentication.js'
-
-app.use(cors());
-app.use(express.json());
 const port = 5000;
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(cors());
+
+import { onUserAuthentication } from './server/authentication/register/authentication.js'
 
 app.use('/Authentication', onUserAuthentication);
 
-dbConnection.on('acquire', function(connection) {
-  console.log('Connection %d acquired', connection.threadId);
-});
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
--- server/authentication/register/authentication.js
+++ server/authentication/register/authentication.js
@@ -1,27 +1,55 @@
 import bcrypt from 'bcrypt';
-
 import dbConnection from '../../database/connection.js';
+import authToken from 'jsonwebtoken';
 
-export async function onUserAuthentication(req, res) {
-    const { email, name, password } = req.body;
-  
+const onClientRegister = async (res, {email, name, password}) => {
     const hashedPassword = bcrypt.hashSync(password, 10);
   
     try {
         const [rows] = await dbConnection.query('SELECT emailAddress from user_accounts WHERE emailAddress = ?', [email]);
         if (rows.length > 0) {
-          res.status(400).json({ error: 'This email already registered!' });
-          return;
+            res.status(400).json({ error: 'This email already registered!' });
+            return;
         }
         
         const insertQuery = `INSERT INTO user_accounts (fullName, userPassword, emailAddress) VALUES (?, ?, ?)`;
         const values = [name, hashedPassword, email];
-        await dbConnection.query(insertQuery, values);
-        
-        res.json({ message: 'Registration successful.' });
-      } catch (err) {
+        await dbConnection.query(insertQuery, values); 
+
+        const token = authToken.sign({ password }, 'secret');
+        res.status(200).send({ token });
+
+    } catch (err) {
         console.error(err);
-        res.status(400).json({ error: 'Something went wrong, please try again later!' });
-      }
-      
+        res.status(200).json({ error: 'Something went wrong, please try again later!' });
+    }
+
+};
+
+const onClientLogin = async (res, {email, password}) => {
+    try {
+        
+        const [rows] = await dbConnection.query('SELECT * FROM WHERE emailAddress = ? AND userPassword = ?', [email, hashedPassword]);
+        if(rows.length > 0) {
+
+            return;
+        }
+
+        res.send(200).json({
+            loginError: 'Invalid email or password. Please try again.'
+        })
+
+    } catch (error) {
+        if(error) throw error;
+    }
+};
+
+export async function onUserAuthentication(req, res) {
+    const authType = req.body.authType;
+
+    if(authType === 'register') {
+        onClientRegister(res, req.body);
+    } else {
+        onClientLogin(res, req.body);
+    }
 }
