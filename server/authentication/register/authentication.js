be86f48e04168588f8b9c1f98660c210bddccbf8
maximdudai <maxim.dudai01@gmail.com>
2023-04-16 22:20:04 +0100

    install module express-session

--- server/authentication/register/authentication.js
+++ server/authentication/register/authentication.js
@@ -1,6 +1,11 @@
-import bcrypt from 'bcrypt';
+import bcrypt, { hash } from 'bcrypt';
 import dbConnection from '../../database/connection.js';
 import authToken from 'jsonwebtoken';
+
+const error_message = {
+    TYPE_DATA: 'Incorrect email address or password.',
+    TYPE_SERVER: 'Internat sever error'
+}
 
 const onClientRegister = async (res, {email, name, password}) => {
     const hashedPassword = bcrypt.hashSync(password, 10);
@@ -8,7 +13,7 @@ const onClientRegister = async (res, {email, name, password}) => {
     try {
         const [rows] = await dbConnection.query('SELECT emailAddress from user_accounts WHERE emailAddress = ?', [email]);
         if (rows.length > 0) {
-            res.status(400).json({ error: 'This email already registered!' });
+            res.status(400).json({ message: 'This email already registered!' });
             return;
         }
         
@@ -21,28 +26,44 @@ const onClientRegister = async (res, {email, name, password}) => {
 
     } catch (err) {
         console.error(err);
-        res.status(200).json({ error: 'Something went wrong, please try again later!' });
+        res.status(200).json({ message: 'Something went wrong, please try again later!' });
     }
 
 };
 
-const onClientLogin = async (res, {email, password}) => {
+const onClientLogin = async (res, { email, password }) => {
     try {
-        
-        const [rows] = await dbConnection.query('SELECT * FROM WHERE emailAddress = ? AND userPassword = ?', [email, hashedPassword]);
+
+        const [rows] = await dbConnection.query('SELECT * FROM user_accounts WHERE emailAddress = ?', [email]);
         if(rows.length > 0) {
+            const data = rows[0];
+            const comparePassword = bcrypt.compareSync(password, data.userPassword);
+            if(!comparePassword) {
+                res.status(400).json({
+                    error_type: error_message.TYPE_DATA
+                });
+                return;
+            }
+            else {
+                const token = authToken.sign({ password }, 'secret');
+                res.status(200).send({ token });
+                return;
+            }
+        } 
 
-            return;
-        }
-
-        res.send(200).json({
-            loginError: 'Invalid email or password. Please try again.'
+        res.status(400).json({
+            message: 'Invalid email or password. Please try again.'
         })
 
     } catch (error) {
-        if(error) throw error;
+        console.log(error);
+
+        res.status(500).json({
+            message: error_message.TYPE_SERVER
+        });
     }
 };
+  
 
 export async function onUserAuthentication(req, res) {
     const authType = req.body.authType;
